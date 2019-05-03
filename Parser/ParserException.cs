aaa21803f3faac8429aa94c62cc218c223173b89
SG_Muwa <motherlode.muwa@gmail.com>
2019-05-03 20:12:02 +0300

    Изменён конструктор в ParserException

--- Parser/ParserException.cs
+++ Parser/ParserException.cs
@@ -1,17 +1,15 @@
-﻿using Lexer;
-using System;
-using System.Collections.Generic;
+﻿using System;
 
 namespace Parser
 {
     public class ParserException : Exception
     {
-        public ParserException(string messsage)
-            : base(messsage) { }
+        public ParserException(string messsage, int IndexTerminalError = -1)
+            : base(messsage + $"IndexTerminalError = {IndexTerminalError}") => this.IndexTerminalError = IndexTerminalError;
         public ParserException()
             : base() { }
 
-        public int EndexTerminalError { get; } = -1;
+        public int IndexTerminalError { get; }
         //public List<Terminal> Expected = ;
     }
 }
