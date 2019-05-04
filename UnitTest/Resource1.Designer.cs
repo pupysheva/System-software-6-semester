1f2c8a548a2f95d59969977efb3daea833352b28
SG_Muwa <motherlode.muwa@gmail.com>
2019-05-04 20:41:15 +0300

    UnitTest теперь для всех проектов.

--- UnitTest/Resource1.Designer.cs
+++ UnitTest/Resource1.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LexerUnitTest {
+namespace UnitTest {
     using System;
     
     
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Text;
 
-namespace LexerUnitTest
+namespace UnitTest
 {
     [TestClass]
     public class UnitTest1
