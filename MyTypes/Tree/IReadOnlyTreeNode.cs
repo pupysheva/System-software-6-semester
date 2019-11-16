e4d523bc0c57b1735f36e7d1d2126f5f8281e17f
SG_Muwa <motherlode.muwa@gmail.com>
2019-11-16 17:13:46 +0300

    Оформление файлов для отправки отчёта об ошибке.

--- MyTypes/Tree/IReadOnlyTreeNode.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace MyTypes.Tree
-{
-    public interface IReadOnlyTreeNode<out T> : IReadOnlyList<IReadOnlyTreeNode<T>>
-    {
-        T Current { get; }
-    }
-}
---
+++ UnitTest/Program.cs
@@ -0,0 +1,6 @@
+namespace MyProject {
+    public class Program
+    {
+        public static void Main(string[] args){}
+    }
+}
